45bb448e90476c9cd8534bd44c36d5db877b348f
James Lyle Young <jameslyleyoung@gmail.com>
2017-03-21 09:07:59 -0500

    code through video 3 of fortune cookie. Start on video 4.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import webapp2
+import random
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("<button>Hello world!!!!</button>")
+        header = "<h1>Fortune Cookie</h1>"
+        lucky_number = random.randint(1, 100)
+        number_sentence = "Your lucky number: " + str(lucky_number)
+        number_paragraph = "<p>" + number_sentence + "</p>"
+        self.response.write(header + number_paragraph)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
